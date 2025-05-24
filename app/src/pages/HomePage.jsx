938f7de41f20e5dcbc9070184e0a3b3d4d86418a
rajatsinghal02 <singhalrajat354@gmail.com>
2025-05-24 14:57:55 +0530

    Feature Section Added

--- app/src/pages/HomePage.jsx
+++ app/src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import HeroSection from '../components/HeroSection'
 import Navbar from '../components/Navbar'
 import Playground from '../components/Playground'
+import Features from '../components/Features'
 
 const HomePage = () => {
   return (
@@ -10,6 +11,7 @@ const HomePage = () => {
         <div className='pt-15'>
       <HeroSection />
       <Playground/>
+      <Features/>
       </div>
     </div>
   )
